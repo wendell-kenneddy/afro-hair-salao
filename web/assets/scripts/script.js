1d16f58ab0dcebedc3aa017bacf5b1d91b42400a
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-10-27 15:10:23 -0300

    feat: rename script.js to main.js

--- web/assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('contact-form');
-const handleSubmit = (e) => {
-  e.preventDefault();
-
-  alert('Feedback recebido!');
-};
-
-form.addEventListener('submit', handleSubmit);
