8c44314c53b9ea3a75d8c5c2c8a0b2901e256604
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-10-28 07:37:37 -0300

    refactor(scripts): merge scripts into main.js

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -1,3 +1,39 @@
-import { App } from './App.js';
+(async () => {
+  const form = document.getElementById('contact-form');
+  const validateForm = () => {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const messageInput = document.getElementById('message');
 
-App.init();
+    if (
+      !nameInput.value ||
+      !emailInput.value ||
+      !phoneInput.value ||
+      !messageInput.value
+    )
+      throw new Error('Por favor, preencha todos os campos.');
+  };
+
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      try {
+        validateForm();
+        alert('Feedback recebido!');
+        form.reset();
+      } catch (error) {
+        alert(error.message);
+      }
+    });
+  }
+
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('/sw.js');
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+})();
