c341dc5efeb0a4fa4b8c4fe1337d1973939f7f97
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-11-02 15:56:45 -0300

    feat: update cache version

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v1';
-const dynamicCacheName = 'dynamic-cache-v1';
+const staticCacheName = 'static-cache-v2';
+const dynamicCacheName = 'dynamic-cache-v2';
 const assets = [
   '/',
   '/index.html',
